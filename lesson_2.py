ce2abc24636dbc6084f0eeb1412955428091b06d
Umut Nebioglu <umutnebioglu@Umuts-Air.mshome.net>
2023-04-24 17:31:59 +0200

    presedence.

--- lesson_2.py
+++ lesson_2.py
@@ -3,5 +3,6 @@ a = 5
 b = 3
 c = 10
 # Compute and print the logical output
-print(a + b * c == 80) # This should produce True
-print(a / b + c == 5 / 13) # This should produce True
+print((a + b) * c == 80) # This should produce True
+print(a / (b + c) == 5 / 13) # This should produce True 
+
