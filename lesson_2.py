caecdbc2758dcdd8727f3ed45f05b3b9f75b6ef7
Umut Nebioglu <umutnebioglu@Umuts-Air.mshome.net>
2023-04-24 16:55:40 +0200

    Added lesson_2.py file

---
+++ lesson_2.py
@@ -0,0 +1,7 @@
+# Define the variables
+a = 5
+b = 3
+c = 10
+# Compute and print the logical output
+print(a + b * c == 80) # This should produce True
+print(a / b + c == 5 / 13) # This should produce True
